4c76e565b64dda1fae8e6761426cca4a04bad384
Menna Alaa <menaalaa460@gmail.com>
2026-09-09 05:51:19 +0300

    Q1

--- C SH-session 5Ass/Program.cs
+++ C SH-session 5Ass/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿#region Q1
+//using System;
+//class book
+//{
+//    private string password = "secret";
+
+//}
+
+//class program
+//{
+//    static void main()
+//    {
+//        Book book = new Book ();
+//        Console.WriteLine (book.password);
+//    }
+//}
+#endregion
